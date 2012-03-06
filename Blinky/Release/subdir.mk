a3666fd4bf57917e2792c1aa4c96a918049bfc16
Jonas Williamsson <williamsson.jonas@gmail.com>
2012-03-06 12:03:10 +0100

    First commit

---
+++ Blinky/Release/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../main.cpp 
+
+OBJS += \
+./main.o 
+
+CPP_DEPS += \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR C++ Compiler'
+	avr-g++ -I"C:\Program Files (x86)\arduino\arduino-1.0\hardware\arduino\cores\arduino" -I"C:\Program Files (x86)\arduino\arduino-1.0\hardware\arduino\variants\standard" -Wall -Os -fpack-struct -fshort-enums -funsigned-char -funsigned-bitfields -fno-exceptions -mmcu=atmega328p -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
